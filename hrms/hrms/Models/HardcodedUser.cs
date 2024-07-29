08eb943bce6ee4f9109164ebc31c52db4af99152
ArbinIndia-Pushkar <pushkar.p@arbin.com>
2024-07-29 12:01:35 +0530

    Add project files.

---
+++ hrms/hrms/Models/HardcodedUser.cs
@@ -0,0 +1,12 @@
+﻿namespace hrms.hrms.Models
+{
+    public static class HardcodedUsers
+    {
+        public static User AdminUser = new User
+        {
+            Username = "admin",
+            Password = "000000"  // In a real application, use a hashed password
+        };
+    }
+
+}
