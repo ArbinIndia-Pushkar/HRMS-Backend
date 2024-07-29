19bb8a73603319b01b72db399d679fd15776b5d4
ArbinIndia-Pushkar <pushkar.p@arbin.com>
2024-07-29 12:07:08 +0530

    added swagger services

--- hrms/Program.cs
+++ hrms/Program.cs
@@ -3,12 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger(); // Enable middleware to serve Swagger UI
+    app.UseSwaggerUI(); // Enable middleware to serve Swagger UI
 }
 else
 {
